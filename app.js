7aa94c722def8ea90456d8ba3221ee18c66a7b44
Jon_shen <jerosshen@gmail.com>
2017-01-18 16:23:35 +0800

    optimize gulpfile

--- app.js
+++ app.js
@@ -1,71 +1,49 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var favicon = require('serve-favicon');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var mongoose = require('mongoose');
-var ueditor = require('ueditor');
-const compression = require('compression');      // 压缩
-var config = require('./config')
+const express = require('express');
+const app = express();
+const path = require('path');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const ueditor = require('ueditor');
+const compression = require('compression');
+const logger = require('morgan');
+const multipart = require('connect-multiparty');
+const cookieParser = require('cookie-parser');   // session 需要用 cookie 配合使用
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);  // 用于本地 session
+const config = require('./config');
+const dburl = 'mongodb://localhost/' + config.name;
 
+// 连接数据库
+mongoose.connect(dburl);
 
-// 开发用的模块
-var logger = require('morgan');                  // HTTP request logger middleware for node.js
-
-var multipart = require('connect-multiparty');
-
-
-//for the offline storage
-var session = require('express-session')
-var MongoStore = require('connect-mongo')(session)     // 用于本地 session
-var cookieParser = require('cookie-parser')
-var dburl = 'mongodb://localhost/' + config.name;
-
-// database
-mongoose.connect(dburl)
-// mongoose.on('error', console.error.bind(console,'连接错误:'));    // 报错，not a function
-
-// program config
-app.set('port', process.env.PORT || config.port);                  // 设置端口号
+// 模板引擎设置
+app.set('port', process.env.PORT || config.port);   // 设置端口号
 app.set('views', './views/pages');
 app.set('view engine', 'pug');
 
+// app 串联各种中间件
 app.use(express.static('public'));
-
 app.use(compression());
-
-
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
-
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));  // key/value 使用
 app.use(bodyParser.json());
-
-
-// 如果有使用 ueditor, 这个上传和 ueditor的上传模块冲突，所以要放在 ueditor后面
-app.use(multipart());
+app.use(multipart());   // 如果有使用 ueditor, 这个上传和 ueditor的上传模块冲突，所以要放在 ueditor后面
 
 // 用户登录持久化
-app.use(cookieParser());        //session 依赖的中间件  存储sessionid
-app.use(session({               //用来本地存储信息 store 对象
+app.use(cookieParser());
+app.use(session({
   secret: config.secret,
-  resave: false,                // 不设置为 true, 登录会失败
+  resave: false,
   saveUninitialized: true,
-  cookie: {
-    secure: false,
-    maxAge: 43200000              // 半个小时
-  },                            // 这次是这里设置为false才登陆成功，resave 设置false， 奇怪
-  store: new MongoStore({
-    url: dburl,
-    collection: 'sessions'      // 这条不懂，为什么是sessions是
-  })
-}))
+  cookie: { secure: false, maxAge: 43200000 },
+  store: new MongoStore({ url: dburl, collection: 'sessions' })
+}));
 
-// 本地变量设置
-app.locals.moment = require('moment')
-// app.locals._env = app.get('env')                   //pod里不能用这个
-app.locals._env = process.env.NODE_ENV                // 判定是否开发环境，是就调用本地资源
-
+// 本地变量设置，用于前段模板文件
+app.locals.moment = require('moment');
+app.locals._env = process.env.NODE_ENV;             // 判定是否开发环境，是就调用本地资源；生产环境则使用 cdn 资源
 
 // 开发使用套件
 if ('development' === app.get('env')){              // 如果是开发环境
@@ -78,8 +56,7 @@ if ('development' === app.get('env')){              // 如果是开发环境
 // 路由
 require('./routes/router')(app);
 
-
 // 最后启动程序
-var server = app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function() {
   console.log('网站程序已启动，端口： ' + server.address().port);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
 'use strict'
 
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var browserSync = require('browser-sync').create('zhongyu');
-// var reload = browserSync.reload;
-var config = require('./config');
-// const path = require('path');
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+const browserSync = require('browser-sync').create('zhongyu');
+const config = require('./config');
 
 // stylus need
-var data = require('gulp-data');
-var stylus = require('gulp-stylus');
-var rename = require('gulp-rename');     // 重命名
-var sourcemaps = require('gulp-sourcemaps');  // 就是开发调试的时候压缩文件映射到源文件
+const data = require('gulp-data');
+const stylus = require('gulp-stylus');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');  // 就是开发调试的时候压缩文件映射到源文件
 
-
+// 错误处理
+const plumber = require('gulp-plumber');
 
 // 路径定义
 var paths = {
@@ -26,7 +25,8 @@ var openBrowser = (process.platform === 'win32')?false:true;
 
 // 编译 stylus，开发模式的调试使用
 gulp.task('stylus2css', function () {
-  return gulp.src('./public/dev/stylus/*.styl')
+  return gulp.src('./public/stylus/*.styl')
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(stylus())
     .pipe(sourcemaps.write())
@@ -35,11 +35,12 @@ gulp.task('stylus2css', function () {
 
 // 压缩且没有映射用于项目 build
 gulp.task('compress', function () {
-  return gulp.src('./public/dev/stylus/main.styl')
+  return gulp.src('./public/stylus/*.styl')
     .pipe(stylus({
       compress: true
     }))
-    .pipe(gulp.dest('./public/stylesheet/build'));
+    .pipe(rename({suffix: '.min',}))
+    .pipe(gulp.dest('./public/stylesheet/'));
 });
 
 // 应用自动重启
@@ -51,24 +52,23 @@ gulp.task('serve', function () {
       './public/',
       './idea/',
     ],
-    // watch:    [paths.routes, paths.models],
     env: { 'NODE_ENV': 'development' }
   })
 })
 
 // 前端页面自动刷新
-gulp.task('start', ['serve'], function() {
+gulp.task('start', function() {
   browserSync.init({      // null 干什么用？
       proxy: 'http://localhost:' + config.port,   // 监控代理地址
-      files: ['./public', './views'],   // 监控的文件
-      open: openBrowser,                 // 是否打开浏览器
-      browser: 'google chrome',         // 打开的浏览器名称
-      notify: false,                    // 浏览器不现实通知，不知道什么意思
-      port: 5000                        // 映射到的地址
+      files: ['./public', './views'],             // 监控的文件
+      open: openBrowser,                          // 是否打开浏览器
+      browser: 'google chrome',                   // 打开的浏览器名称
+      notify: false,                              // 浏览器不现实通知，不知道什么意思
+      port: 5000                                  // 映射到的地址
   });
-  gulp.watch('./dev_public/stylus/*.styl', ['stylus2css']);
+  gulp.watch('./util/dev_public/stylus/*.styl', ['stylus2css']);
 })
 
 
 gulp.task('build', ['compress'])
-// gulp.task('default', ['start', 'css'])
+gulp.task('default', ['serve', 'start'])
