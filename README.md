60df1407c9d843575880c7b9d62a5ef0fa5ea9ef
jerossh <jerosshen@gmail.com>
2017-04-07 00:24:24 +0800

    优化缓存策略， 增加 yarn 的支持

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# express init
+# 技术栈
 
 This program creates by express + jade + mongodb + stylus(optional)
 
-## Let's get started
+本应用已包含了大部分 express 需要的资源
+
+- favicon 图标
+- 数据库连接
+- 多媒体表格
+- 响应压缩
+- 离线策略
+- 安全策略
+- 缓存策略
+- 开发者模式
+- 自动化
+
+## 如何使用
+
+```
+git clone https://github.com/jerossh/express-init.git
+cd express-init
+git remote remove origin
+```
+然后你可以添加到自己的仓库
 
 ```
 bower install
@@ -13,7 +32,7 @@ gulp
 ## History
 
 * 2016-11-28  Finish v0.0.1  init
-* 2017-01-18  Finish v0.1.0  optimize gulpfile
-
+* 2017-01-18  Finish v0.1.0  优化 gulp 策略
+* 2017-04-07  Finish v0.2.0  优化缓存策略， 增加 yarn 的支持
 ## License
 MIT
--- config.js
+++ config.js
@@ -1,7 +1,28 @@
-module.exports = {
+const serveStatic = require('serve-static');
+
+function setCustomCacheControl(res, path) {
+    if (serveStatic.mime.lookup(path) === 'text/html') {
+        // Custom Cache-Control for HTML files
+        res.setHeader('Cache-Control', 'public, max-age=0');
+    }
+}
+
+const staticOption = {
+    dotfiles: 'ignore',
+    etag: true,
+    extensions: false,
+    index: false,
+    lastModified: true,
+    maxAge: '1d',
+    redirect: true,
+    setHeaders: setCustomCacheControl
+};
+
+const c = {
   'name': 'type-here-program-name',
   'secret': 'program',
   'port': 3000,
+  'staticOption': staticOption,
   'programSetting': {
 
   }
